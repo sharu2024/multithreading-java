12f2b6cdf3d0b06dc28ddc811f8e960b941ab26f
Sharuhashini Pichaipillai <113116215+sharu2024@users.noreply.github.com>
2024-02-26 15:20:18 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,10 @@
+package mutlithreading;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		Thread t1=Thread.currentThread();
+		System.out.println("Current Thread"+t1);
+	}
+
+}
